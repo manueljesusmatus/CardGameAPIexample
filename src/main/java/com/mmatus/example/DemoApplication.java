b2fcd6e39a515680843e9f3c70363add0f398652
manueljmatus <manueljesusmatus@gmail.com>
2020-08-16 17:14:57 -0400

    [ADD] update tree directory

--- src/main/java/com/mmatus/example/DemoApplication.java
+++ src/main/java/com/mmatus/example/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.mmatus.example.example;
+package com.mmatus.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
