9a35b74617063b7d15921166696c874c69f22b13
manueljmatus <manueljesusmatus@gmail.com>
2020-08-31 21:06:47 -0400

    [ADD] price data retrieve

---
+++ example/src/main/java/com/mmatus/example/Controller/APIpricesController.java
@@ -0,0 +1,39 @@
+package com.mmatus.example.Controller;
+
+import java.util.List;
+
+import com.mmatus.example.Entities.Price;
+import com.mmatus.example.Services.PriceServ;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("PriceController")
+@RequestMapping("/api/v1")
+public class APIpricesController {
+
+    @Autowired
+	@Qualifier("PriceService")
+    private PriceServ priceServ;
+
+    @GetMapping("/PriceInfo")
+    public ResponseEntity<Object> getPriceInfo(
+                        @RequestParam(value = "id", required = false) Long id )
+        {
+        List<Price> listCard;
+        if(id != null){
+            Price instancia = priceServ.readPriceByIdCard(id);
+            return new ResponseEntity<Object>(instancia, HttpStatus.OK);
+        }else{
+            listCard = priceServ.readPriceInfo();
+            return new ResponseEntity<Object>(listCard, HttpStatus.OK);
+        }
+    }
+
+}
